19bf5f7990c672633a837d4ff7d677a26b2cfa39
isteven <isteven@server.fake>
2014-05-09 19:03:20 +0800

    work in progress for new features

--- angular-multi-select.js
+++ angular-multi-select.js
@@ -51,21 +51,28 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
             orientation     : '@',
             maxLabels       : '@',
             isDisabled      : '=',
-            directiveId     : '@'
+            directiveId     : '@',            
+            // https://github.com/isteven/angular-multi-select/pull/5             
+            onOpen          : '&',
+            onClose         : '&',
+            // TODO 
+            onBlur          : '&',
+            onFocus         : '&',
+            helperButtons   : '@'
         },
 
         template: 
-            '<span class="multiSelect inlineBlock">' +
+            '<span class="multiSelect inlineBlock" >' +
                 '<button type="button" class="multiSelect button multiSelectButton" ng-click="toggleCheckboxes( $event ); refreshSelectedItems();" ng-bind-html="varButtonLabel">' +
                 '</button>' +                
                 '<div class="multiSelect checkboxLayer hide">' +
                     '<div class="multiSelect line">' +
-                        '<span ng-if="!isDisabled">Select: &nbsp;</span>' + 
-                            '<button type="button" ng-click="select( \'all\' )"    class="multiSelect helperButton" ng-if="!isDisabled && selectionMode.toUpperCase() != \'SINGLE\'">All</button> ' +
-                            '<button type="button" ng-click="select( \'none\' )"   class="multiSelect helperButton" ng-if="!isDisabled && selectionMode.toUpperCase() != \'SINGLE\'">None</button> ' + 
-                            '<button type="button" ng-click="select( \'reset\' )"  class="multiSelect helperButton" ng-if="!isDisabled">Reset</button>' +
+                        '<span ng-if="!isDisabled && ( helperButtons2.toUpperCase().indexOf( \'ALL\') >= 0 || helperButtons2.toUpperCase().indexOf( \'NONE\') >= 0 || helperButtons2.toUpperCase().indexOf( \'RESET\') >= 0 )">Select: &nbsp;</span>' + 
+                            '<button type="button" ng-click="select( \'all\' )"    class="multiSelect helperButton" ng-if="!isDisabled && selectionMode.toUpperCase() != \'SINGLE\' && helperButtons2.toUpperCase().indexOf( \'ALL\') >= 0 ">All</button> ' +
+                            '<button type="button" ng-click="select( \'none\' )"   class="multiSelect helperButton" ng-if="!isDisabled && selectionMode.toUpperCase() != \'SINGLE\' && helperButtons2.toUpperCase().indexOf( \'NONE\') >= 0 ">None</button> ' + 
+                            '<button type="button" ng-click="select( \'reset\' )"  class="multiSelect helperButton" ng-if="!isDisabled && helperButtons2.toUpperCase().indexOf( \'RESET\') >= 0 "">Reset</button>' +
                     '</div>' +
-                    '<div class="multiSelect line">' + 
+                    '<div class="multiSelect line" ng-if="helperButtons2.toUpperCase().indexOf( \'FILTER\') >= 0">' + 
                         'Filter: <input class="multiSelect" type="text" ng-model="labelFilter" />' +
                             '&nbsp;<button type="button" class="multiSelect helperButton" ng-click="labelFilter=\'\'">Clear</button>' +
                     '</div>' +
@@ -76,7 +83,7 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                         '<div class="multiSelect acol">' +
                             '<label class="multiSelect" ng-class="{checkboxSelected:item[ tickProperty ]}">' +
                                 '<input class="multiSelect checkbox" type="checkbox" ng-disabled="itemIsDisabled( item )" ng-checked="item[ tickProperty ]" ng-click="syncItems( item, $event )" />' +
-                                    '<span class="multiSelect" ng-class="{disabled:itemIsDisabled( item )}" ng-bind-html="writeLabel( item, \'itemLabel\' )"></span>' +
+                                '<span class="multiSelect" ng-class="{disabled:itemIsDisabled( item )}" ng-bind-html="writeLabel( item, \'itemLabel\' )"></span>' +
                             '</label>&nbsp;&nbsp;' +
                         '</div>' +
                     '</div>' +
@@ -87,9 +94,13 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
             
             $scope.selectedItems    = [];    
             $scope.backUp           = [];
-            $scope.varButtonLabel   = '';            
+            $scope.varButtonLabel   = '';   
+            $scope.tabIndex         = 0;
+            $scope.tabables         = null;
+            $scope.currentButton    = null;
+            $scope.helperButtons2   = '';
 
-            // Checkbox is ticked
+            // When a checkbox is ticked...
             $scope.syncItems = function( item, e ) {                                                                
                 index = $scope.inputModel.indexOf( item );                
                 $scope.inputModel[ index ][ $scope.tickProperty ]   = !$scope.inputModel[ index ][ $scope.tickProperty ];
@@ -104,7 +115,7 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                     }        
                     $scope.toggleCheckboxes( e );
                 }
-
+                
                 $scope.refreshSelectedItems();                   
             }     
 
@@ -196,7 +207,7 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                 return $sce.trustAsHtml( label );
             }
 
-            // UI operations to show/hide checkboxes
+            // UI operations to show/hide checkboxes based on click event..
             $scope.toggleCheckboxes = function( e ) {                                                
 
                 if ( e.target ) {                    
@@ -235,15 +246,24 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                     for( i=0; i < checkboxes.length; i++ ) {
                         if ( i != multiSelectIndex ) {
                             checkboxes[i].className = 'multiSelect checkboxLayer hide';
+                            // JH DotComIt 5/8/2014 Added method handler for closing the popup                            
+                            removeKeyListener( multiSelectButtons[ multiSelectIndex ] );
                         }
                     }                    
 
                     if ( checkboxes[ multiSelectIndex ].className == 'multiSelect checkboxLayer hide' ) {                    
+                        $scope.currentButton = multiSelectButtons[ multiSelectIndex ];
                         checkboxes[ multiSelectIndex ].className = 'multiSelect checkboxLayer show';
+                        // JH DotComIt 5/8/2014 Added method handler for opening the popup                        
+                        $scope.onOpen();
+                        addKeyListener( multiSelectButtons[ multiSelectIndex ] );
                     }
                     else if ( checkboxes[ multiSelectIndex ].className == 'multiSelect checkboxLayer show' ) {                                    
                         checkboxes[ multiSelectIndex ].className = 'multiSelect checkboxLayer hide';
-                    }                
+                        // JH DotComIt 5/8/2014 Added method handler for closing the popup
+                        $scope.onClose();
+                        removeKeyListener( multiSelectButtons[ multiSelectIndex ] );
+                    }
                 }
             }
 
@@ -280,8 +300,14 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                             }
                         });                
                         break;      
-                    case 'RESET':
-                        $scope.inputModel = angular.copy( $scope.backUp );                        
+                    case 'RESET':     
+                        var ctr = 0;
+                        // The reset method is a bit worrying.. 
+                        // What if one day a browser decides to sort object on their own?
+                        angular.forEach( $scope.backUp, function( value, key ) {
+                            $scope.inputModel[ ctr ][ $scope.tickProperty ] = value[ $scope.tickProperty ];                            
+                            ctr++;
+                        });
                         break;
                     default:                        
                 }
@@ -290,6 +316,7 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
 
 
             // Generic validation for required attributes
+            // Might give false positives so just ignore if everything's alright.
             validate = function() {
                 if ( !( 'inputModel' in attrs )) {
                     console.log( 'Multi-select error: input-model is not defined! (ID: ' + $scope.directiveId + ')' );
@@ -328,8 +355,99 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                 });    
                 if ( notThere === true ) {
                     console.log( 'Multi-select error: property "' + missingLabel + '" is not available in the input model. (Name: ' + $scope.directiveId + ')' );
-                }
+                }                
+            }
+
+            // Button listeners 
+            buttonListener = function( keyPressEvent ) {
+
+                nextElement = this.nextSibling.querySelectorAll( 'button, input' );        
                 
+                // Down or right or tab
+                if ( keyPressEvent.keyCode === 40 || keyPressEvent.keyCode === 39 || (keyPressEvent.keyCode === 9 && !keyPressEvent.shiftKey) ) {
+                    nextElement[ 0 ].focus();
+                    $scope.tabIndex = 0;                        
+                    keyPressEvent.preventDefault();
+                    keyPressEvent.stopPropagation();
+                }
+
+                // Up or left or shift+tab
+                else if ( keyPressEvent.keyCode === 38 || keyPressEvent.keyCode === 37 || (keyPressEvent.keyCode === 9 && keyPressEvent.shiftKey) ) {
+                    nextElement[ nextElement.length - 1 ].focus();
+                    $scope.tabIndex = nextElement.length - 1;                        
+                    keyPressEvent.preventDefault();
+                    keyPressEvent.stopPropagation();
+                }
+
+                // Esc
+                else if ( keyPressEvent.keyCode === 27 ) {                        
+                    var checkboxes = document.querySelectorAll( '.checkboxLayer' );
+                    for( i=0; i < checkboxes.length; i++ ) {
+                        checkboxes[ i ].className = 'multiSelect checkboxLayer hide';
+                    }
+                    $scope.tabIndex = 0;
+                }
+            }
+
+            // Checkbox listeners
+            checkboxListener = function( keyPressEvent ) {
+                
+                // Down or right or tab                
+                if ( keyPressEvent.keyCode === 40 || keyPressEvent.keyCode === 39 || (keyPressEvent.keyCode === 9 && !keyPressEvent.shiftKey) ) {                    
+                    $scope.tabIndex++;
+                    if ( $scope.tabIndex > $scope.tabables.length - 1) { 
+                        $scope.tabIndex = 0;
+                    }                            
+                    $scope.tabables[ $scope.tabIndex ].focus();                                                        
+                    keyPressEvent.preventDefault();
+                    keyPressEvent.stopPropagation();                
+                }
+
+                // Up or left or shift+tab
+                else if ( keyPressEvent.keyCode === 38 || keyPressEvent.keyCode === 37 || (keyPressEvent.keyCode === 9 && keyPressEvent.shiftKey) ) {
+                    $scope.tabIndex--;
+                    if ( $scope.tabIndex < 0 ) { 
+                        $scope.tabIndex = $scope.tabables.length - 1;
+                    }                            
+                    $scope.tabables[ $scope.tabIndex ].focus();                                                        
+                    keyPressEvent.preventDefault();
+                    keyPressEvent.stopPropagation();                
+                }
+
+                // Esc
+                else if ( keyPressEvent.keyCode === 27 ) {
+                    var checkboxes = document.querySelectorAll( '.checkboxLayer' );
+                    for( i=0; i < checkboxes.length; i++ ) {
+                        checkboxes[ i ].className = 'multiSelect checkboxLayer hide';
+                    }
+                    $scope.tabIndex = 0;
+                }                                        
+            }
+
+            // Register listeners
+            addKeyListener = function( element ) { 
+
+                $scope.tabIndex = 0;
+
+                // Button                
+                element.addEventListener( 'keypress', buttonListener, false );
+
+                // Checkboxes
+                $scope.tabables = element.nextSibling.querySelectorAll( 'button, input' );                
+                for( i = 0 ; i < $scope.tabables.length ; i++ ) {
+                    $scope.tabables[ i ].addEventListener( 'keypress', checkboxListener, false );
+                }
+            }
+
+            // Remove key listener 
+            removeKeyListener = function( element ) {
+                // Button
+                element.removeEventListener( 'keypress', buttonListener );
+                // Checkboxes
+                $scope.tabables = element.nextSibling.querySelectorAll( 'button, input' );                
+                for( i = 0 ; i < $scope.tabables.length ; i++ ) {
+                    $scope.tabables[ i ].removeEventListener( 'keypress', checkboxListener, false );
+                }                
             }
 
             ///////////////////////
@@ -337,7 +455,12 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
             ///////////////////////               
 
             validate();
-            $scope.refreshSelectedItems();                  
+            $scope.refreshSelectedItems();   
+
+            // If helperButtons is not defined, we show all helpers
+            if ( typeof $scope.helperButtons === 'undefined' ) {
+                $scope.helperButtons2 = 'all none reset filter';
+            }
 
             // Watch for changes in input model (allow dynamic input)
             $scope.$watch( 'inputModel' , function( oldVal, newVal ) {                 
@@ -359,12 +482,18 @@ angular.module( 'multi-select', ['ng'] ).directive( 'multiSelect' , [ '$sce', fu
                 var checkboxes = document.querySelectorAll( '.checkboxLayer' );     
                 if ( e.target.className.indexOf( 'multiSelect' ) === -1 ) {
                     for( i=0; i < checkboxes.length; i++ ) {                                        
-                        checkboxes[i].className = 'multiSelect checkboxLayer hide';                        
-                    }    
+                        checkboxes[i].className = 'multiSelect checkboxLayer hide';
+                        // JH DotComIt 5/8/2014 Added method handler for closing the popup
+                        // Call on blur but just once                        
+                        if ( i === 0 ) {
+                            $scope.onBlur();
+                        }
+                        
+                    }
                     e.stopPropagation();
                 }                                
-            });           
-            
+            });             
+                    
             // For IE8, perhaps. Not sure if this is really executed.
             if ( !Array.prototype.indexOf ) {
                 Array.prototype.indexOf = function(what, i) {                    
