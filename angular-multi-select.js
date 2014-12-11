b6da86838ed5a2dfd40ffd67d003e271e82a5b0f
Paul Degenkolbe <paul.degenkolbe@4k-analytics.de>
2014-12-11 15:20:11 +0100

    compile directive -  for changes

--- angular-multi-select.js
+++ angular-multi-select.js
@@ -34,7 +34,7 @@
 angular.module( 'multi-select', ['ng'] )
     .directive('compile', ['$compile', function ($compile) {
         return function(scope, element, attrs) {
-            var ensureCompileRunsOnce = scope.$watch(
+            scope.$watch(
                 function(scope) {
                     // watch the 'compile' expression for changes
                     return scope.$eval(attrs.compile);
@@ -43,13 +43,10 @@ angular.module( 'multi-select', ['ng'] )
                     // when the 'compile' expression changes
                     // assign it into the current DOM
                     element.html(value);
-
-                    // compile the new DOM and link it to the current
-                    // scope.
+                    // compile the new DOM and link it to the current scope.
                     // NOTE: we only compile .childNodes so that
                     // we don't get into infinite loop compiling ourselves
                     $compile(element.contents())(scope);
-                    ensureCompileRunsOnce();
                 }
             );
         };
