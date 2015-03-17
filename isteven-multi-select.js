524e837758f352b3f560538bbda13d1ea2218b41
Andrew Westberg <andrew@swiftmako.com>
2015-03-17 13:13:41 -0400

    allow literal chars in button and item labels

--- isteven-multi-select.js
+++ isteven-multi-select.js
@@ -582,7 +582,8 @@ angular.module( 'isteven-multi-select', ['ng'] ).directive( 'istevenMultiSelect'
                 angular.forEach( temp, function( value, key ) {                    
                     item[ value ] && ( label += '&nbsp;' + value.split( '.' ).reduce( function( prev, current ) {
                         return prev[ current ]; 
-                    }, item ));        
+                    }, item ));       
+		    !item[ value ] && (label += value); 
                 });
                 if ( type.toUpperCase() === 'BUTTONLABEL' ) {
                     return label;
